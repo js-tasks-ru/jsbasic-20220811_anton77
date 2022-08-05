56f95f315c10751d9b27805936ecb3f30869e29f
Anton Krasnikov <antkras20@gmail.com>
2022-08-05 23:46:02 +0300

    Tasks 0.1, 1.1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0) {
+    return 1;
+}
+  else {
+    let result = n;
+    for (let i = 1; i < n; i++) {
+    result = result*(n - i);    
+    }
+    return result;
+  }
 }
