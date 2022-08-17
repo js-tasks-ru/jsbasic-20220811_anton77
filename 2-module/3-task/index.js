05c6ce582b51e58301754e7b4a7a82de40d5589c
Anton Krasnikov <antkras20@gmail.com>
2022-08-17 19:16:21 +0300

    2-module 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
 let calculator = {
-  // ваш код
-};
-
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+  a:0,
+  b:0,
+  read: function(a,b){
+    this.a = a;
+    this.b = b;
+  },
+  sum: function(){
+    return this.a + this.b;
+  },
+  mul: function(){
+    return this.a * this.b;
+  }
+  };
+  
+  // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
+  window.calculator = calculator; // делает ваш калькулятор доступным глобально
