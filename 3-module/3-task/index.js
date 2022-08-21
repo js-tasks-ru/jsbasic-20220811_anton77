813c654536553d1c2a6f74eeb6474f2b9178f2b2
Anton Krasnikov <antkras20@gmail.com>
2022-08-21 19:18:08 +0300

    3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let array = str.split('-');
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+      let newFirstSymbolItem = array[i][0].toUpperCase();
+      let newItem = array[i].replace(array[i][0], newFirstSymbolItem);
+      result += newItem;
+  }
+  return(result);
 }
