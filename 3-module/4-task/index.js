8658ffa68a9d3c73afbb05af633a341066323fbb
Anton Krasnikov <antkras20@gmail.com>
2022-08-22 20:25:28 +0300

    3-module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,18 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultArray = [];
+  for (let item = 0; item < users.length; item++){
+      if(users[item]['age'] <= age){
+          resultArray.push(users[item]);
+      };
+  };
+  let resultstr = '';
+  for (let item = 0; item < resultArray.length; item++) {
+      if(item == (resultArray.length - 1)){
+          resultstr += (resultArray[item]['name'] + ', ' + resultArray[item]['balance']); 
+      }
+      else{
+          resultstr += (resultArray[item]['name'] + ', ' + resultArray[item]['balance'] + "\n");  
+      };
+  };
+  return(resultstr);
 }
